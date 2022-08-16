45781c76dab0b84459b3c874c83013c14b8070d9
Colmc2966 <colmc2966@gmail.com>
2022-08-16 22:02:08 +0000

    Creating layout and functions to be defined

--- run.py
+++ run.py
@@ -2,11 +2,21 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+
 PLAYER_BOARD = [[' '] * 8 for x in range(6)] 
 CPU_BOARD = [[' '] * 8 for x in range(6)]
 
 guessing_code = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5}
 
 def board():
+    pass
+
+def create_ship_locations():
+    pass
+
+def user_guesses():
+    pass
+
+def game_tracker():
     pass
 
