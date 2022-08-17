5c4cd4f8f3ed07bca2576d7614b6d7fa79889233
Colmc2966 <colmc2966@gmail.com>
2022-08-17 06:21:23 +0000

    adding row '6' to valid inputs

--- run.py
+++ run.py
@@ -8,11 +8,11 @@ from random import randint
 PLAYER_BOARD = [[' '] * 6 for x in range(6)]
 CPU_BOARD = [[' '] * 6 for x in range(6)]
 
-guessing_code = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5}
+guessing_code = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5 }
 
 
 def print_board(board):
-    print('  A B C D E F ')
+    print('  A B C D E F  ')
     print('  -----------')
     row_number = 1
     for row in board:
@@ -30,7 +30,7 @@ def create_ship_locations(board):
 
 def user_guesses():
     row = input('Enter a ship row 1-6')
-    while row not in '12345':
+    while row not in '123456':
         print('Enter a valid ship row')
         row = input('Enter a ship row 1-6')
     column = input('Enter a ship column A-F').upper()
